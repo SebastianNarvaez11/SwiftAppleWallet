9f3c8305321bdf02867934bf073cc370383abce7
Sebastian Narvaez <sebastian.narvaez@aplicando.co>
2024-10-08 16:38:48 -0500

    Initial Commit

---
+++ AppleWallet/AppleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWalletApp.swift
+//  AppleWallet
+//
+//  Created by Sebastian on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
